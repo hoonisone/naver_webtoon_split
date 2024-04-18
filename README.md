8dfb31c1836d2448ede94f13369114a49f6b9081
hoonison <hoonisone@gmail.com>
2024-04-18 21:38:43 +0900

    preprocessing & readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # webtoon_dataset
 네이버 웹툰 크롤링 &amp; 이미지 편집 &amp; 레이블링
+* 네이버 웹툰은 이미지 크롤링이 가능
+* 
+
+# Crawling
+* 각각의 컷이 일정한 길이로 분할된다.
+* 따라서 글자가 잘리는 부분이 생긴다.
+
+#### 처리전
+![origin_1](images/origin_1.jpg)
+![origin_2](images/origin_2.jpg)
+
+# Preprocessing
+* 이미지 가로 길이 맞추기 (예외적으로 크기가 다른 경우가 있음)
+
+# Stitching
+cv 라이브러리로 vertical stitching 수행
+
+# Spliting
+흰색 선을 기준으로 split 수행
+
+#### 처리후
+![result_1](images/result_1.png)
+![result_2](images/result_2.png)
+![result_3](images/result_3.png)
--- cartoon_editor.py
+++ cartoon_editor.py
@@ -3,6 +3,7 @@ import re
 from PIL import Image
 import cv2
 import numpy as np
+from collections import Counter
 
 class CartoonImageEditor:
     def __init__(self, min_cut_size = None, max_cut_size=None):
@@ -17,13 +18,42 @@ class CartoonImageEditor:
         files = sorted(files, key = lambda x : self.extract_order(str(x)))
         images = [Image.open(file) for file in files]
         return images
-        
+    
+    def get_width(self, images):
+        # images = PIL.Image list
+        # image_list로 부터 width 계산
+        # 각 cut의 width가 하나로 통일되지 않은 경우가 있어 common width를 찾아 반환
+        width_list = [img.size[0] for img in images]
+        counter = Counter(width_list)
+        return counter.most_common()[0][0]
+    
+    def fit_width(self, images, width):
+        # width값을 하나로 통일
+        new_images = []
+        for i, img in enumerate(images):
+            w, h = img.size
+            if w != width:
+                new_images.append(img.resize((width, h)))
+            else:
+                new_images.append(img)
+        return new_images
+    
+    def harmonize_width(self, images):
+        width = self.get_width(images)
+        return self.fit_width(images, width)
+    
+    def preprocessing(self, images):
+        return self.harmonize_width(images)
+    
     def image_stitch(self, images, split_line = False, split_line_thickness=3):
-        images = [np.array(img) for img in images]
+        images = [np.array(img) for img in images]        
+        black_line = images[0][:split_line_thickness, :, :]*0
+        
+        
         if split_line:
-            black_line = images[0][:split_line_thickness, :, :]*0
             images = [[image, black_line.copy()]for image in images]
             images = sum(images, [])[:-1]
+            
         return Image.fromarray(cv2.vconcat(images))
 
     def extract_split_ranges(self, image:Image):
@@ -62,3 +92,17 @@ class CartoonImageEditor:
         title.mkdir(parents=True, exist_ok=True)
         for i, image in enumerate(images):
             image.save(title/f"cut_{i+1}.png")
+            
+    def restiching_single_episode(self, dir_path):
+        work_name = dir_path.parent.name
+        episode_name = dir_path.name
+        images = self.load_images(dir_path)
+        images = self.preprocessing(images)
+        image = self.image_stitch(images)
+        ranges = self.extract_split_ranges(image)
+        images = self.image_split(image, ranges)
+        
+        Path(f"resource/results/{work_name}/{episode_name}").mkdir(parents=True, exist_ok=True)
+        image.save(f"resource/results/{work_name}/{episode_name}/full.png")
+        self.image_stitch(images, split_line=True).save(f"resource/results/{work_name}/{episode_name}/lined_full.png")
+        self.save_images(images, f"resource/results/{work_name}/{episode_name}")
